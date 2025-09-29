f2cb64fc052a82e5ccc96fafd8c516ac6eb88885
SachinthaAbeygunasekara <112684802+SachinthaAbeygunasekara@users.noreply.github.com>
2025-09-29 17:11:33 +0530

    feat: create project file structure and add home page

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+import view.HomePageForm;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Sachintha
+ */
+public class Main {
+    public static void main(String[] args) {
+        new HomePageForm().setVisible(true);
+    }
+}
